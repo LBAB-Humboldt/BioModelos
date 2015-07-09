873420939b73810eb54f78e3bcfc3c838c432d29
CAScgutierrez <cgutierrez@humboldt.org.co>
2015-07-09 15:13:15 -0500

    indice de grupos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
 Rails.application.config.assets.precompile += %w( leaflet/Google.min.js )
+Rails.application.config.assets.precompile += %w( leaflet/leaflet.draw.0.2.3dev.min.js )
+Rails.application.config.assets.precompile += %w( mapEditor.js )
